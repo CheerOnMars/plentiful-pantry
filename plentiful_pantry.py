47414d7980b9197b65ddaec6d0277ce35c7a2b82
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-07-13 05:52:02 -0700

    added sqlite db and migration files

--- plentiful_pantry.py
+++ plentiful_pantry.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import Recipe, Ingredient, Instruction
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Recipe': Recipe, 'Ingredient': Ingredient, 'Instruction': Instruction}
