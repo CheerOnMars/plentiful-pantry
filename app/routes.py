f44ad33f4c1d202e22b0b72f14a257a04bd7f113
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-07-17 16:33:40 -0700

    add /recipes route

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,32 @@
-"""
-This module will provide RESTful endpoints for all requests
-necessary to fulfill the requirements.
-"""
 import os
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.models import Recipe, Ingredient
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.models import Recipe, Ingredient, Instruction, Category
 
 # import pdb; pdb.set_trace()
 
 import json
 with open('data/recipes.json') as f:
-    recipes_struct = json.load(f)
+    json_recipes_struct = json.load(f)
 
 import csv
 with open('data/ingredients.csv', 'r') as f:
     reader = csv.reader(f)
-    ingredients_struct = list(reader)
+    csv_ingredients_struct = list(reader)
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Super Sario'}
-    return render_template('index.html', title='Home', user=user, recipes=recipes_struct)
+    return render_template('index.html', title='Home', user=user, recipes=json_recipes_struct)
 
 @app.route('/ingredients')
 def ingredients():
     user = {'username': 'Super Sario'}
-    return render_template('ingredient_list.html', title='Home', user=user, ingredients=ingredients_struct)
+    return render_template('ingredient_list.html', title='Ingredients', user=user, ingredients=csv_ingredients_struct)
+
+@app.route('/recipes')
+def recipes():
+    user = {'username': 'Super Sario'}
+    recipes = Recipe.query.order_by(Recipe.name.asc())
+    return render_template('recipes.html', title='Recipes', user=user, recipes=recipes)
