15050db84c21910c08a20541c6071482ce68564c
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-07-17 16:29:56 -0700

    update database seed file to add additional fields from recipe json file

--- data/data_scripts/database_seed.py
+++ data/data_scripts/database_seed.py
@@ -2,13 +2,11 @@ import os
 import json
 from app import db, models as m
 
-
 def get_recipes():
     recipes_file = os.path.join(os.path.dirname(__file__), '..', 'recipes.json')
     # import pdb; pdb.set_trace()
     with open(recipes_file) as f:
         return json.load(f)
-
 
 def create_or_retrieve_ingredient(record):
     """Create or retrieve an ingredient.
@@ -16,13 +14,27 @@ def create_or_retrieve_ingredient(record):
     If the ingredient exists load and return the Model instance.
     Otherwise create the ingredient and return the Model instance.
     """
-    ing = m.Ingredient.query.filter_by(ingredient_name=record).first()
+    ing = m.Ingredient.query.filter_by(name=record).first()
     if not ing:
-        ing = m.Ingredient(ingredient_name=record)
+        ing = m.Ingredient(name=record)
         db.session.add(ing)
         db.session.commit()
 
     return ing
+
+def create_or_retrieve_category(record):
+    """Create or retrieve an category.
+
+    If the category exists load and return the Model instance.
+    Otherwise create the ingredicategoryent and return the Model instance.
+    """
+    cat = m.Category.query.filter_by(name=record).first()
+    if not cat:
+        cat = m.Category(name=record)
+        db.session.add(cat)
+        db.session.commit()
+
+    return cat
 
 for recipe in get_recipes():
     # print(f"Creating recipe {recipe['name']}")
@@ -34,13 +46,17 @@ for recipe in get_recipes():
     instructions = "\n".join(recipe['recipeInstructions'])
 
     rec = m.Recipe(
-        recipe_name=recipe['name'],
-        description=recipe['description'],
-        recipe_category=recipe['recipeCategory'],
+        name=recipe['name'],
         image=recipe['image'],
+        description=recipe['description'],
+        category=recipe['recipeCategory'],
+        source = "Good and Cheap by Leann Brown",
         recipe_yield=recipe['recipeYield'],
+        url=recipe['recipeURL'],
+        is_included=True,
+        # instruction_list=recipe['recipeInstructions'],
         ingredients=ingredients,
-        instructions=[m.Instruction(instruction_text=instructions)]
+        instructions=[m.Instruction(text=instructions)]
     )
     db.session.add(rec)
     db.session.commit()
