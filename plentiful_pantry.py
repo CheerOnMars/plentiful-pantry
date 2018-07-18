5688d4ddf294554e1bfe3a6abafb5617ac5c22c3
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-07-17 17:13:20 -0700

    add category to list of things to access

--- plentiful_pantry.py
+++ plentiful_pantry.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import Recipe, Ingredient, Instruction
+from app.models import Recipe, Ingredient, Instruction, Category
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Recipe': Recipe, 'Ingredient': Ingredient, 'Instruction': Instruction}
+    return {'db': db, 'Recipe': Recipe, 'Ingredient': Ingredient, 'Instruction': Instruction, 'Category': Category}
