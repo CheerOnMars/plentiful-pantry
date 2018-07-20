9d95cc8e47481794eaa72cf8432c49f982d98470
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-07-20 02:08:48 -0700

    add RecipeIngredient to import on app and routes

--- plentiful_pantry.py
+++ plentiful_pantry.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import Recipe, Ingredient, Instruction, Category
+from app.models import Recipe, Ingredient, Instruction, Category, RecipeIngredient
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Recipe': Recipe, 'Ingredient': Ingredient, 'Instruction': Instruction, 'Category': Category}
+    return {'db': db, 'Recipe': Recipe, 'Ingredient': Ingredient, 'Instruction': Instruction, 'Category': Category, 'RecipeIngredient': RecipeIngredient}
