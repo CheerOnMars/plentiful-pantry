01104e14f35fcd05e8ac4feff27b41bb5e2437dd
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-07-21 12:02:44 -0700

    got inventory seed to work

--- data/data_scripts/seed_inventory_temp.py
+++ data/data_scripts/seed_inventory_temp.py
@@ -16,12 +16,13 @@ def get_inventory():
 for item in get_inventory():
     option = item[0]
     presence = True if item[2] == "True" else False
-    ing = m.Ingredient.query.filter(m.Ingredient.name == option).first()
+    ing = m.Ingredient.query.filter_by(name=option).first()
     inv = m.Inventory(
-        ingredient_id=ing,
+        ingredient=ing,
         is_present=presence,
     )
     print (inv)
+    print (ing)
     print (inv.ingredient_id)
     print (inv.is_present)
     import pdb; pdb.set_trace()
